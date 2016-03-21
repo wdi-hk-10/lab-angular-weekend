2848278466e42c91c8934bca6d4675d04cb8c6f4
Denis Cheung <d.s.h.cheung@gmail.com>
2016-03-21 11:16:58 +0800

    math solution

--- ten-seconds-math/js/application.js
+++ ten-seconds-math/js/application.js
@@ -1,1 +1,43 @@
 // Your Code Here
+var app = angular.module('10SecondsMath', []);
+
+app.controller('ClockCtrl', ['$scope', '$interval', function($scope, $interval){
+  var countDown;
+  var generateRandom = function (max) {
+    max = max || 10;
+    return Math.floor(Math.random() * max + 1);
+  };
+  var createInterval = function () {
+    return $interval(function(){
+      $scope.secondsLeft--;
+      if ($scope.secondsLeft === 0) {
+        $scope.gameover = true;
+        $interval.cancel(countDown);
+      }
+    }, 1000);
+  };
+  $scope.evalAnswer = function () {
+    if ($scope.userInput == $scope.num1 + $scope.num2) {
+      if ($scope.started === false) {
+        countDown = createInterval();
+        $scope.started = true;
+      }
+      $scope.points++;
+      $scope.secondsLeft++;
+      $scope.num1 = generateRandom();
+      $scope.num2 = generateRandom();
+      $scope.userInput = '';
+    }
+  };
+
+  $scope.init = function () {
+    $scope.started = false;
+    $scope.gameover = false;
+    $scope.points = 0;
+    $scope.secondsLeft = 2;
+    $scope.num1 = generateRandom();
+    $scope.num2 = generateRandom();
+  };
+
+  $scope.init();
+}]);
