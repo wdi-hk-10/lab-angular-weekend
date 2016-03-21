6ae74bd12c31f251820f42286276600d20f082fa
Denis Cheung <d.s.h.cheung@gmail.com>
2016-03-21 09:59:49 +0800

    solution object style

--- shopping-cart/js/application.js
+++ shopping-cart/js/application.js
@@ -1,1 +1,35 @@
 // Your Code Here
+var app = angular.module('ShoppingCart', []);
+
+app.controller('CartCtrl', ['$scope', function($scope){
+  $scope.lists = {
+    'apple': {
+      price: 1.99,
+      quantity: 10
+    },
+    'orange': {
+      price: 1.99,
+      quantity: 5
+    }
+  };
+  // $scope.newItem = {}
+  $scope.addItem = function () {
+    if ($scope.lists[$scope.newItem.name]) {
+      $scope.lists[$scope.newItem.name].price = $scope.newItem.price;
+    } else {
+      $scope.lists[$scope.newItem.name] = {
+        price: $scope.newItem.price,
+        quantity: 0
+      };
+    }
+  };
+
+  $scope.totalPrice = function () {
+    var total = 0;
+    for (var key in $scope.lists) {
+      var valueObj = $scope.lists[key];
+      total += valueObj.price * valueObj.quantity;
+    }
+    return total;
+  };
+}]);
